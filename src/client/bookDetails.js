51dbbab38edb6927a6af4d48fd0109e43ef8c740
breadbun407 <121478122+breadbun407@users.noreply.github.com>
2023-07-12 05:43:19 +0000

    Creating search

--- src/client/bookDetails.js
+++ src/client/bookDetails.js
@@ -1,24 +1,4 @@
-const books = [
-    {
-      title: 'Book 1',
-      author: 'Author 1',
-      datePublished: '2022-01-01',
-      summary: 'Summary of Book 1',
-      rating: 4.5,
-      lastRead: '2022-07-01',
-      review: 'Review of Book 1'
-    },
-    {
-      title: 'Book 2',
-      author: 'Author 2',
-      datePublished: '2022-02-01',
-      summary: 'Summary of Book 2',
-      rating: 3.8,
-      lastRead: '2022-06-15',
-      review: 'Review of Book 2'
-    },
-    // Add more book objects as needed
-  ];
+const books = [];
   
 function saveLocal(){
     localStorage.setItem('books', JSON.stringify(books));
