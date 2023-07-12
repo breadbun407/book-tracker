da0199aa81594e96f5e5ab5ea13cba254ea0283b
breadbun407 <121478122+breadbun407@users.noreply.github.com>
2023-07-12 05:14:44 +0000

    setting up save and load - localstorage

---
+++ src/client/bookdetails.js
@@ -0,0 +1,40 @@
+const books = [
+    {
+      title: 'Book 1',
+      author: 'Author 1',
+      datePublished: '2022-01-01',
+      summary: 'Summary of Book 1',
+      rating: 4.5,
+      lastRead: '2022-07-01',
+      review: 'Review of Book 1'
+    },
+    {
+      title: 'Book 2',
+      author: 'Author 2',
+      datePublished: '2022-02-01',
+      summary: 'Summary of Book 2',
+      rating: 3.8,
+      lastRead: '2022-06-15',
+      review: 'Review of Book 2'
+    },
+    // Add more book objects as needed
+  ];
+  
+function save(){
+    localStorage.setItem('books', JSON.stringify(books));
+  }
+
+function loadLocal(){
+    // Retrieve books array from localStorage
+const storedBooks = JSON.parse(localStorage.getItem('books'));
+
+// Check if storedBooks is not null (localStorage exists)
+    if (storedBooks !== null) {
+  // Use the retrieved array of books
+  console.log(storedBooks);
+} else {
+  // Handle case where there are no stored books
+  console.log('No books found in localStorage');
+}
+
+  }
