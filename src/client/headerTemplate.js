25cfe3d7a15134ebaf32a4c418b17056fec40608
Bread <121478122+breadbun407@users.noreply.github.com>
2023-07-13 00:53:21 +0000

    A functional navbar toggle - just ugly tho

--- src/client/headerTemplate.js
+++ src/client/headerTemplate.js
@@ -2,25 +2,35 @@ const header = document.createElement("header");
 
 header.innerHTML = `
   <h1> Golden Book Trove</h1>
-    <nav class="navbar">
-        <ul class="navbar-nav">
-          <li class="nav-item">
-            <a class="nav-link" href="index.html">Home</a>
-          </li>
-          <li class="nav-item">
-            <a class="nav-link" href="#">Books</a>
-          </li>
-          <li class="nav-item">
-            <a class="nav-link" href="search.html">Search</a>
-          </li>
-          <li class="nav-item">
-            <a class="nav-link" href="addABook.html">Add a book</a>
-          </li>
-        </ul>
-        <div class="navbar-toggle">
-    <span class="toggle-icon" id="navbarToggle"></span>
-  </div>
-      </nav>
+  <nav class="navbar">
+    <ul class="navbar-nav">
+      <li class="nav-item">
+        <a class="nav-link" href="index.html">Home</a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="#">Books</a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="search.html">Search</a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="addABook.html">Add a book</a>
+      </li>
+    </ul>
+    <div class="navbar-toggle">
+      <span class="toggle-icon" id="navbarToggle"></span>
+    </div>
+  </nav>
 `;
 
 document.querySelector("body > div.header").appendChild(header);
+
+const navbarToggle = document.getElementById("navbarToggle");
+const navbarNav = document.querySelector(".navbar-nav"); // Updated selector
+
+document.addEventListener("DOMContentLoaded", function () {
+  navbarToggle.addEventListener("click", function () {
+    navbarToggle.classList.toggle("active");
+    navbarNav.classList.toggle("active");
+  });
+});
--- src/client/index.js
+++ src/client/index.js
@@ -1,7 +1,1 @@
-const navbarToggle = document.getElementById("navbarToggle");
-const navbarNav = document.getElementById("navbarNav");
 
-navbarToggle.addEventListener("click", function () {
-  navbarToggle.classList.toggle("active");
-  navbarNav.classList.toggle("active");
-});
